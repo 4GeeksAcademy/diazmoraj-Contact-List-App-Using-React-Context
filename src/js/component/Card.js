7e94d5489e3a0f849e28375984a4072ea9639c33
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-07-30 23:45:32 +0000

    Creacion de componente Card

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -4,7 +4,14 @@ import {Context} from "./../store/appContext"
 const Card = () => {
     const {store, actions} = useContext(Context)
     return(
-        <h1>Card Component</h1>
+        <div className="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
     )
 }
 
